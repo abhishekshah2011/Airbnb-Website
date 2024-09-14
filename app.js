892c9235ef145a6b88ae7f2d8dfe0c9901d0ce3e
Abhishek Shah <abhishekshah7452@gmail.com>
2024-09-14 14:57:50 +0530

    commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -22,6 +23,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 
 app.get("/", (req, res) => {
@@ -69,7 +72,15 @@ app.get("/listings/:id/edit",async (req, res) => {
 app.put("/listings/:id",async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, {...req.body.listing});
-  res.redirect("listings");
+  res.redirect("/listings");
+});
+
+//Delete Route
+app.delete("/listings/:id",async (req, res) =>  {
+  let  { id } = req.params;
+  let deleteListing = await Listing.findByIdAndDelete(id)
+  console.log(deleteListing);
+  res.redirect("/listings");
 });
 
 
